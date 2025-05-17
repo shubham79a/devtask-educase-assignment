26aa736d97f0901f0ac461a712a884e322bbea18
shubham79a <shubh79.nolan@gmail.com>
2025-05-17 18:33:59 +0530

    added home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
 function App() {
 
   return (
-    <>
-    <h1 className="bg-amber-200">hello</h1>
-    </>
+    <Routes>
+      <Route path='/' element={<Home />} />
+    </Routes>
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+    return (
+        <div className='max-w-xs max-sm:w-[280px] mx-auto h-[90vh] border border-gray-200 flex flex-col justify-end my-[5vh] rounded-md shadow' >
+            <div className='flex flex-col gap-5 p-5 '>
+                <div className='flex flex-col gap-3'>
+                    <h1 className='font-extrabold text-2xl'> Welcome to Popx </h1>
+                    <p className='text-base text-gray-500 leading-[19px] '>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis, possimus.</p>
+                </div>
+                <Link to="/signup">
+                    <button className='w-full bg-[#6c25ff] rounded-md py-1.5 text-white cursor-pointer'>
+                        Create Account
+                    </button>
+                </Link>
+                <Link to="/login">
+                    <button className='w-full bg-[#cfb9fb] text-black font-medium  rounded-md py-1.5 cursor-pointer'>
+                        Already Registered? Login
+                    </button>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default Home
